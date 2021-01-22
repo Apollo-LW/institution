524950eabf36dead0d36ab1ac62866907202ca43
mrmoon <abuamaramfreelance@gmail.com>
2021-01-23 01:17:55 +0200

    commit add Institution room

--- src/main/java/com/apollo/institution/model/Institution.java
+++ src/main/java/com/apollo/institution/model/Institution.java
@@ -9,13 +9,11 @@ public class Institution {
 
     private Boolean isActive = true, isPublic = false;
     private HashSet<String> institutionCourse = new HashSet<>();
-    private HashSet<String> institutionAdmins = new HashSet<>();
-    private HashSet<String> institutionParents = new HashSet<>();
-    private HashSet<String> institutionMembers = new HashSet<>();
-    private HashSet<String> institutionChildren = new HashSet<>();
+    private HashSet<String> institutionAdmins = new HashSet<>() , institutionParents = new HashSet<>();
+    private HashSet<String> institutionMembers = new HashSet<>() , institutionChildren = new HashSet<>();
     private final String institutionId = UUID.randomUUID().toString();
     private final Date institutionDateOfCreation = Calendar.getInstance().getTime();
-    private String institutionName = this.institutionId + '-' + this.institutionDateOfCreation;
+    private String institutionName = this.institutionId + '-' + this.institutionDateOfCreation , institutionRoomId;
 
     public Institution addCourseById(String courseId) {
         this.institutionCourse.add(courseId);
